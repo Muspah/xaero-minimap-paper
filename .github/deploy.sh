4cecdd609bfff53d54103f8f516318b1946f6156
Muspah <simondw@sapo.pt>
2025-03-30 20:50:47 +0200

    Right directory now?

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -5,10 +5,10 @@ echo -e "Publishing artifacts...\n"
 cd $HOME
 
 rsync -r --quiet -e "ssh -p 7685 -o StrictHostKeyChecking=no" \
-$HOME/work/Muspah/xaero-minimap-paper/build/repo/ \
+$HOME/work/work/xaero-minimap-paper/xaero-minimap-paper/build/repo/ \
 u36810p330294@uskyblock.ovh:/repo.biemcraft.nl/public_html/repo/
 
 echo -e "Publishing final plugin release...\n"
 rsync -r --quiet -e "ssh -p 7685 -o StrictHostKeyChecking=no" \
-$HOME/work/Muspah/xaero-minimap-paper/build/libs/xaero-minimap-paper-*.jar \
+$HOME/work/work/xaero-minimap-paper/xaero-minimap-paper/build/libs/xaero-minimap-paper-*.jar \
 u36810p330294@uskyblock.ovh:domains/repo.biemcraft.nl/public_html/downloads/xaero-minimap-paper/
